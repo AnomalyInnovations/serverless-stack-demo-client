c573a118b1cdf66e5d69da60a5bac159d0633c7a
Jay <jvelayud@gmail.com>
2017-03-19 21:25:24 -0400

    Updating

--- src/Routes.js
+++ src/Routes.js
@@ -5,6 +5,7 @@ import AppliedRoute from './components/AppliedRoute';
 import Home from './containers/Home';
 import Login from './containers/Login';
 import Signup from './containers/Signup';
+import NewNote from './containers/NewNote';
 import NotFound from './containers/NotFound';
 
 export default ({ childProps }) => (
@@ -12,6 +13,7 @@ export default ({ childProps }) => (
     <AppliedRoute path="/" exact component={Home} props={childProps} />
     <AppliedRoute path="/login" exact component={Login} props={childProps} />
     <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
+    <AppliedRoute path="/notes/new" exact component={NewNote} props={childProps} />
     { /* Finally, catch all unmatched routes */ }
     <Route component={NotFound} />
   </Switch>
--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
   cognito: {
     USER_POOL_ID : 'us-east-1_YEhdMFvix',
     APP_CLIENT_ID : '29qpcqorti0ctjkur5h01dph5g',
