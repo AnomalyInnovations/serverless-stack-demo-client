57d631b997b26d8020ae706f9faf5f3ca6c11689
Jay <jvelayud@gmail.com>
2017-03-19 22:37:13 -0400

    Updating

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
 import {
   PageHeader,
   ListGroup,
@@ -70,6 +70,10 @@ class Home extends Component {
       <div className="lander">
         <h1>Scratch</h1>
         <p>A simple note taking app</p>
+        <div>
+          <Link to="/login" className="btn btn-info btn-lg">Login</Link>
+          <Link to="/signup" className="btn btn-success btn-lg">Signup</Link>
+        </div>
       </div>
     );
   }
