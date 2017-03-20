5b4e60e9ab493e1e3edd9ad5e33ca750db710010
Jay <jvelayud@gmail.com>
2017-03-19 20:34:26 -0400

    Updating

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Router>
+    <App />
+  </Router>,
   document.getElementById('root')
 );
