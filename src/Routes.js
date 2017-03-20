c3a1b3f02976a3a9331a75fb0c9eb8e68067977e
Jay <jvelayud@gmail.com>
2017-03-19 20:47:50 -0400

    Updating

--- src/Routes.js
+++ src/Routes.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Home from './containers/Home';
+import NotFound from './containers/NotFound';
 
 export default () => (
   <Switch>
     <Route path="/" exact component={Home} />
+    { /* Finally, catch all unmatched routes */ }
+    <Route component={NotFound} />
   </Switch>
 );
