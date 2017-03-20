96346a03bb3ed82ced59ed1bd3aa899d60381785
Jay <jvelayud@gmail.com>
2017-03-19 20:40:15 -0400

    Updating

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Link } from 'react-router-dom';
+import { Navbar } from 'react-bootstrap';
+import Routes from './Routes';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App container">
+        <Navbar fluid collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to="/">Scratch</Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+        </Navbar>
+        <Routes />
       </div>
     );
   }
---
+++ src/containers/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>Scratch</h1>
+          <p>A simple note taking app</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
