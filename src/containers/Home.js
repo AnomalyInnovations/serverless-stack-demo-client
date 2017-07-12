8e24a154b4cc460278ef4705e6238bd54303b9d3
Jay <jvelayud@gmail.com>
2017-07-12 15:17:49 -0400

    Updating to IAM

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,17 +1,56 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import {
+  PageHeader,
+  ListGroup,
+} from 'react-bootstrap';
 import './Home.css';
 
 class Home extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: false,
+      notes: [],
+    };
+  }
+
+  renderNotesList(notes) {
+    return null;
+  }
+
+  renderLander() {
+    return (
+      <div className="lander">
+        <h1>Scratch</h1>
+        <p>A simple note taking app</p>
+      </div>
+    );
+  }
+
+  renderNotes() {
+    return (
+      <div className="notes">
+        <PageHeader>Your Notes</PageHeader>
+        <ListGroup>
+          { ! this.state.isLoading
+            && this.renderNotesList(this.state.notes) }
+        </ListGroup>
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className="Home">
-        <div className="lander">
-          <h1>Scratch</h1>
-          <p>A simple note taking app</p>
-        </div>
+        { this.props.userToken === null
+          ? this.renderLander()
+          : this.renderNotes() }
       </div>
     );
   }
 }
 
-export default Home;
+export default withRouter(Home);
