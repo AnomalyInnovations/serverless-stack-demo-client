891c0d213e9093191e5740d6bbd7cd1de051aad7
Jay <jvelayud@gmail.com>
2017-07-12 15:38:06 -0400

    Updating to IAM

---
+++ src/components/AuthenticatedRoute.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+export default ({ component: C, props: cProps, ...rest }) => (
+  <Route {...rest} render={props => (
+    cProps.userToken !== null
+      ? <C {...props} {...cProps} />
+      : <Redirect to={`/login?redirect=${props.location.pathname}${props.location.search}`} />
+  )}/>
+);
