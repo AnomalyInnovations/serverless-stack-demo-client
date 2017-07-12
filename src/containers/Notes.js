922ddc11d688e1c5425f7520be75bfbc6fc81e49
Jay <jvelayud@gmail.com>
2017-07-12 15:31:47 -0400

    Updating to IAM

--- src/containers/Notes.js
+++ src/containers/Notes.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
+import {
+  FormGroup,
+  FormControl,
+  ControlLabel,
+} from 'react-bootstrap';
 import { invokeApig } from '../libs/awsLib';
+import LoaderButton from '../components/LoaderButton';
+import config from '../config.js';
+import './Notes.css';
 
 class Notes extends Component {
   constructor(props) {
@@ -9,6 +17,8 @@ class Notes extends Component {
     this.file = null;
 
     this.state = {
+      isLoading: null,
+      isDeleting: null,
       note: null,
       content: '',
     };
@@ -31,11 +41,96 @@ class Notes extends Component {
     return invokeApig({ path: `/notes/${this.props.match.params.id}` }, this.props.userToken);
   }
 
+  validateForm() {
+    return this.state.content.length > 0;
+  }
+
+  formatFilename(str) {
+    return (str.length < 50)
+      ? str
+      : str.substr(0, 20) + '...' + str.substr(str.length - 20, str.length);
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
+  handleFileChange = (event) => {
+    this.file = event.target.files[0];
+  }
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (this.file && this.file.size > config.MAX_ATTACHMENT_SIZE) {
+      alert('Please pick a file smaller than 5MB');
+      return;
+    }
+
+    this.setState({ isLoading: true });
+  }
+
+  handleDelete = async (event) => {
+    event.preventDefault();
+
+    const confirmed = window.confirm('Are you sure you want to delete this note?');
+
+    if ( ! confirmed) {
+      return;
+    }
+
+    this.setState({ isDeleting: true });
+  }
+
   render() {
     return (
       <div className="Notes">
-      </div>
-    );
+        { this.state.note &&
+          ( <form onSubmit={this.handleSubmit}>
+              <FormGroup controlId="content">
+                <FormControl
+                  onChange={this.handleChange}
+                  value={this.state.content}
+                  componentClass="textarea" />
+              </FormGroup>
+              { this.state.note.attachment &&
+              ( <FormGroup>
+                <ControlLabel>Attachment</ControlLabel>
+                <FormControl.Static>
+                  <a target="_blank" rel="noopener noreferrer" href={ this.state.note.attachment }>
+                    { this.formatFilename(this.state.note.attachment) }
+                  </a>
+                </FormControl.Static>
+              </FormGroup> )}
+              <FormGroup controlId="file">
+                { ! this.state.note.attachment &&
+                <ControlLabel>Attachment</ControlLabel> }
+                <FormControl
+                  onChange={this.handleFileChange}
+                  type="file" />
+              </FormGroup>
+              <LoaderButton
+                block
+                bsStyle="primary"
+                bsSize="large"
+                disabled={ ! this.validateForm() }
+                type="submit"
+                isLoading={this.state.isLoading}
+                text="Save"
+                loadingText="Saving…" />
+              <LoaderButton
+                block
+                bsStyle="danger"
+                bsSize="large"
+                isLoading={this.state.isDeleting}
+                onClick={this.handleDelete}
+                  text="Delete"
+                  loadingText="Deleting…" />
+            </form> )}
+        </div>
+      );
   }
 }
 
