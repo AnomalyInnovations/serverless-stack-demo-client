f563c6c1ec444ad754c6fcf032cfbb2cca5368a7
Jay <jvelayud@gmail.com>
2017-07-12 15:04:37 -0400

    Updating to IAM

--- src/config.js
+++ src/config.js
@@ -1,6 +1,12 @@
 export default {
   MAX_ATTACHMENT_SIZE: 5000000,
+  apiGateway: {
+    URL: 'https://w5z1hloaz5.execute-api.us-east-1.amazonaws.com/test',
+    REGION: 'us-east-1',
+  },
   cognito: {
+    REGION: 'us-east-1',
+    IDENTITY_POOL_ID: '29qpcqorti0ctjkur5h01dph5g',
     USER_POOL_ID : 'us-east-1_YEhdMFvix',
     APP_CLIENT_ID : '29qpcqorti0ctjkur5h01dph5g',
   }
---
+++ src/libs/awsLib.js
@@ -0,0 +1,63 @@
+import AWS from 'aws-sdk';
+import sigV4Client from './sigV4Client';
+import config from '../config.js';
+
+export async function invokeApig(
+  { path,
+    method = 'GET',
+    headers = {},
+    queryParams = {},
+    body }, userToken) {
+
+  await getAwsCredentials(userToken);
+
+  const signedRequest = sigV4Client
+    .newClient({
+      accessKey: AWS.config.credentials.accessKeyId,
+      secretKey: AWS.config.credentials.secretAccessKey,
+      sessionToken: AWS.config.credentials.sessionToken,
+      region: config.apiGateway.REGION,
+      endpoint: config.apiGateway.URL,
+    })
+    .signRequest({
+      method,
+      path,
+      headers,
+      queryParams,
+      body
+    });
+
+  body = body ? JSON.stringify(body) : body;
+  headers = signedRequest.headers;
+
+  const results = await fetch(signedRequest.url, {
+    method,
+    headers,
+    body
+  });
+
+  if (results.status !== 200) {
+    throw new Error(await results.text());
+  }
+
+  return results.json();
+}
+
+export function getAwsCredentials(userToken) {
+  if (AWS.config.credentials && Date.now() < AWS.config.credentials.expireTime - 60000) {
+    return;
+  }
+
+  const authenticator = `cognito-idp.${config.cognito.REGION}.amazonaws.com/${config.cognito.USER_POOL_ID}`;
+
+  AWS.config.update({ region: config.cognito.REGION });
+
+  AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: config.cognito.IDENTITY_POOL_ID,
+    Logins: {
+      [authenticator]: userToken
+    }
+  });
+
+  return AWS.config.credentials.getPromise();
+}
