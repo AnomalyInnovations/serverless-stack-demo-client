413549cf6b23384eb432c5e62d37bf352231459e
Jay <jvelayud@gmail.com>
2017-08-17 13:03:11 -0400

    Upgrading

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Navbar } from "react-bootstrap";
+import { Nav, Navbar } from "react-bootstrap";
 import Routes from "./Routes";
+import RouteNavItem from "./components/RouteNavItem";
 import "./App.css";
 
 class App extends Component {
@@ -15,6 +16,12 @@ class App extends Component {
             </Navbar.Brand>
             <Navbar.Toggle />
           </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <RouteNavItem href="/signup">Signup</RouteNavItem>
+              <RouteNavItem href="/login">Login</RouteNavItem>
+            </Nav>
+          </Navbar.Collapse>
         </Navbar>
         <Routes />
       </div>
