b211a1277350a4f5e903cdcca1bbd5ffa714c568
Jay <jvelayud@gmail.com>
2017-08-18 18:43:06 -0400

    Upgrading

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,14 +1,45 @@
 import React, { Component } from "react";
+import { PageHeader, ListGroup } from "react-bootstrap";
 import "./Home.css";
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      notes: []
+    };
+  }
+
+  renderNotesList(notes) {
+    return null;
+  }
+
+  renderLander() {
+    return (
+      <div className="lander">
+        <h1>Scratch</h1>
+        <p>A simple note taking app</p>
+      </div>
+    );
+  }
+
+  renderNotes() {
+    return (
+      <div className="notes">
+        <PageHeader>Your Notes</PageHeader>
+        <ListGroup>
+          {!this.state.isLoading && this.renderNotesList(this.state.notes)}
+        </ListGroup>
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className="Home">
-        <div className="lander">
-          <h1>Scratch</h1>
-          <p>A simple note taking app</p>
-        </div>
+        {this.props.isAuthenticated ? this.renderNotes() : this.renderLander()}
       </div>
     );
   }
