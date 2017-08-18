979bb075c89395f814535d0cd7cfb740476d7a25
Jay <jvelayud@gmail.com>
2017-08-18 18:45:13 -0400

    Upgrading

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { PageHeader, ListGroup } from "react-bootstrap";
+import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
+import { invokeApig } from '../libs/awsLib';
 import "./Home.css";
 
 export default class Home extends Component {
@@ -12,8 +13,52 @@ export default class Home extends Component {
     };
   }
 
+  async componentDidMount() {
+    if (!this.props.isAuthenticated) {
+      return;
+    }
+
+    try {
+      const results = await this.notes();
+      this.setState({ notes: results });
+    } catch (e) {
+      alert(e);
+    }
+
+    this.setState({ isLoading: false });
+  }
+
+  notes() {
+    return invokeApig({ path: "/notes" });
+  }
+
+  handleNoteClick = event => {
+    event.preventDefault();
+    this.props.history.push(event.currentTarget.getAttribute("href"));
+  }
+
   renderNotesList(notes) {
-    return null;
+    return [{}].concat(notes).map(
+      (note, i) =>
+        i !== 0
+          ? <ListGroupItem
+              key={note.noteId}
+              href={`/notes/${note.noteId}`}
+              onClick={this.handleNoteClick}
+              header={note.content.trim().split("\n")[0]}
+            >
+              {"Created: " + new Date(note.createdAt).toLocaleString()}
+            </ListGroupItem>
+          : <ListGroupItem
+              key="new"
+              href="/notes/new"
+              onClick={this.handleNoteClick}
+            >
+              <h4>
+                <b>{"\uFF0B"}</b> Create a new note
+              </h4>
+            </ListGroupItem>
+    );
   }
 
   renderLander() {
