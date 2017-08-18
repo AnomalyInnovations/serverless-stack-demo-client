b123c02ac769d3ae60e92b36beaae859f873e8a5
Jay <jvelayud@gmail.com>
2017-08-18 19:00:58 -0400

    Upgrading

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -55,7 +55,6 @@ export default class Login extends Component {
     try {
       await this.login(this.state.email, this.state.password);
       this.props.userHasAuthenticated(true);
-      this.props.history.push("/");
     } catch (e) {
       alert(e);
       this.setState({ isLoading: false });
