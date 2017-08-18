1ccb1f8124e644eaf89f4a87a06f0f8b416965ef
Jay <jvelayud@gmail.com>
2017-08-18 18:42:04 -0400

    Upgrading

--- src/libs/awsLib.js
+++ src/libs/awsLib.js
@@ -96,6 +96,11 @@ export function signOutUser() {
   if (currentUser !== null) {
     currentUser.signOut();
   }
+
+  if (AWS.config.credentials) {
+    AWS.config.credentials.clearCachedId();
+    AWS.config.credentials = new AWS.CognitoIdentityCredentials({});
+  }
 }
 
 function getUserToken(currentUser) {
