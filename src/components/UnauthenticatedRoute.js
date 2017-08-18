067d20d60ce93cbbb0792eb948b548581217532c
Jay <jvelayud@gmail.com>
2017-08-18 18:57:06 -0400

    Upgrading

---
+++ src/components/UnauthenticatedRoute.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+
+export default ({ component: C, props: cProps, ...rest }) =>
+  <Route
+    {...rest}
+    render={props =>
+      !cProps.isAuthenticated
+        ? <C {...props} {...cProps} />
+        : <Redirect to="/" />}
+  />;
