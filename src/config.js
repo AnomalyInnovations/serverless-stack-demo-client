79ab3658c9910b2dba7135f88d987b497bcf34bc
Jay <jvelayud@gmail.com>
2017-08-18 18:34:12 -0400

    Upgrading

--- src/config.js
+++ src/config.js
@@ -1,7 +1,13 @@
 export default {
   MAX_ATTACHMENT_SIZE: 5000000,
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://5by75p4gn3.execute-api.us-east-1.amazonaws.com/prod"
+  },
   cognito: {
+    REGION: "us-east-1",
     USER_POOL_ID: "us-east-1_udmFFSb92",
-    APP_CLIENT_ID: "4hmari2sqvskrup67crkqa4rmo"
+    APP_CLIENT_ID: "4hmari2sqvskrup67crkqa4rmo",
+    IDENTITY_POOL_ID: "us-east-1:ceef8ccc-0a19-4616-9067-854dc69c2d82"
   }
 };
--- src/libs/awsLib.js
+++ src/libs/awsLib.js
@@ -1,14 +1,68 @@
+import AWS from "aws-sdk";
 import { CognitoUserPool } from "amazon-cognito-identity-js";
+import sigV4Client from "./sigV4Client";
 import config from "../config";
 
+export async function invokeApig({
+  path,
+  method = "GET",
+  headers = {},
+  queryParams = {},
+  body
+}) {
+  if (!await authUser()) {
+    throw new Error("User is not logged in");
+  }
+
+  const signedRequest = sigV4Client
+    .newClient({
+      accessKey: AWS.config.credentials.accessKeyId,
+      secretKey: AWS.config.credentials.secretAccessKey,
+      sessionToken: AWS.config.credentials.sessionToken,
+      region: config.apiGateway.REGION,
+      endpoint: config.apiGateway.URL
+    })
+    .signRequest({
+      method,
+      path,
+      headers,
+      queryParams,
+      body
+    });
+
+  body = body ? JSON.stringify(body) : body;
+  headers = signedRequest.headers;
+
+  const results = await fetch(signedRequest.url, {
+    method,
+    headers,
+    body
+  });
+
+  if (results.status !== 200) {
+    throw new Error(await results.text());
+  }
+
+  return results.json();
+}
+
 export async function authUser() {
+  if (
+    AWS.config.credentials &&
+    Date.now() < AWS.config.credentials.expireTime - 60000
+  ) {
+    return true;
+  }
+
   const currentUser = getCurrentUser();
 
   if (currentUser === null) {
     return false;
   }
 
-  await getUserToken(currentUser);
+  const userToken = await getUserToken(currentUser);
+
+  await getAwsCredentials(userToken);
 
   return true;
 }
@@ -39,4 +93,20 @@ function getCurrentUser() {
     ClientId: config.cognito.APP_CLIENT_ID
   });
   return userPool.getCurrentUser();
+}
+
+function getAwsCredentials(userToken) {
+  const authenticator = `cognito-idp.${config.cognito
+    .REGION}.amazonaws.com/${config.cognito.USER_POOL_ID}`;
+
+  AWS.config.update({ region: config.cognito.REGION });
+
+  AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: config.cognito.IDENTITY_POOL_ID,
+    Logins: {
+      [authenticator]: userToken
+    }
+  });
+
+  return AWS.config.credentials.getPromise();
 }
