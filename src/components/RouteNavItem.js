2938e18b00c641a39091e60d057a90f19827c5f2
Ian Daniel <idaniel@spinnys.com>
2017-09-02 18:45:06 +1000

    Remove the anonymous function from onClick
    Remove the anonymous function from the onClick to its' own memory pointer. 
    This is to remove the possibility of a memory leak and a performance hit.

--- src/components/RouteNavItem.js
+++ src/components/RouteNavItem.js
@@ -2,13 +2,17 @@ import React from "react";
 import { Route } from "react-router-dom";
 import { NavItem } from "react-bootstrap";
 
+const handleClick = (history, event) => {
+    history.push(event.currentTarget.getAttribute("href"))
+}
+
 export default props =>
   <Route
     path={props.href}
     exact
     children={({ match, history }) =>
       <NavItem
-        onClick={e => history.push(e.currentTarget.getAttribute("href"))}
+        onClick={handleClick.bind(this, history)}
         {...props}
         active={match ? true : false}
       >
