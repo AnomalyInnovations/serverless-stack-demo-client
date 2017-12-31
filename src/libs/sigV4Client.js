08bce12337748f20d2656afb5b4ce9ce064fbdd8
Jay <jvelayud@gmail.com>
2017-12-31 17:44:40 -0500

    Upgading to React 16

--- src/libs/sigV4Client.js
+++ src/libs/sigV4Client.js
@@ -153,6 +153,22 @@ sigV4Client.newClient = function(config) {
     return hexEncode(hmac(key, stringToSign));
   }
 
+  function extractHostname(url) {
+    var hostname;
+
+    if (url.indexOf("://") > -1) {
+      hostname = url.split('/')[2];
+    }
+    else {
+      hostname = url.split('/')[0];
+    }
+
+    hostname = hostname.split(':')[0];
+    hostname = hostname.split('?')[0];
+
+    return hostname;
+  }
+
   function buildAuthorizationHeader(
     accessKey,
     credentialScope,
@@ -228,8 +244,7 @@ sigV4Client.newClient = function(config) {
       .replace(/\.\d{3}Z$/, "Z")
       .replace(/[:-]|\.\d{3}/g, "");
     headers[X_AMZ_DATE] = datetime;
-    let parser = new URL(awsSigV4Client.endpoint);
-    headers[HOST] = parser.hostname;
+    headers[HOST] = extractHostname(awsSigV4Client.endpoint);
 
     let canonicalRequest = buildCanonicalRequest(
       verb,
