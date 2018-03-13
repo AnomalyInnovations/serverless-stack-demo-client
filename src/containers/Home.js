a023e08b54b92fb40fecb2804f059a0c71d4bcdd
Jay <jvelayud@gmail.com>
2018-03-13 16:55:31 -0400

    Initial commit

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { API } from "aws-amplify";
+import { Link } from "react-router-dom";
 import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
 import "./Home.css";
 
@@ -66,6 +67,14 @@ export default class Home extends Component {
       <div className="lander">
         <h1>Scratch</h1>
         <p>A simple note taking app</p>
+        <div>
+          <Link to="/login" className="btn btn-info btn-lg">
+            Login
+          </Link>
+          <Link to="/signup" className="btn btn-success btn-lg">
+            Signup
+          </Link>
+        </div>
       </div>
     );
   }
