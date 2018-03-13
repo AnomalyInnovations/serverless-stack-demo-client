f71b67c2ae0f28494a50fc03da8baaeddbc5eeb8
Jay <jvelayud@gmail.com>
2018-03-13 15:47:24 -0400

    Initial commit

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
+import Notes from "./containers/Notes";
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
 import NewNote from "./containers/NewNote";
@@ -13,6 +14,7 @@ export default ({ childProps }) =>
     <AppliedRoute path="/login" exact component={Login} props={childProps} />
     <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
     <AppliedRoute path="/notes/new" exact component={NewNote} props={childProps} />
+    <AppliedRoute path="/notes/:id" exact component={Notes} props={childProps} />
     { /* Finally, catch all unmatched routes */ }
     <Route component={NotFound} />
   </Switch>;
---
+++ src/containers/Notes.js
@@ -0,0 +1,179 @@
+import React, { Component } from "react";
+import { API, Storage } from "aws-amplify";
+import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import LoaderButton from "../components/LoaderButton";
+import { s3Upload } from "../libs/awsLib";
+import config from "../config";
+import "./Notes.css";
+
+export default class Notes extends Component {
+  constructor(props) {
+    super(props);
+
+    this.file = null;
+
+    this.state = {
+      isLoading: null,
+      isDeleting: null,
+      note: null,
+      content: "",
+      attachmentURL: null
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      let attachmentURL;
+      const note = await this.getNote();
+      const { content, attachment } = note;
+
+      if (attachment) {
+        attachmentURL = await Storage.vault.get(attachment);
+      }
+
+      this.setState({
+        note,
+        content,
+        attachmentURL
+      });
+    } catch (e) {
+      alert(e);
+    }
+  }
+
+  getNote() {
+    return API.get("notes", `/notes/${this.props.match.params.id}`);
+  }
+
+  saveNote(note) {
+    return API.put("notes", `/notes/${this.props.match.params.id}`, {
+      body: note
+    });
+  }
+
+  deleteNote() {
+    return API.del("notes", `/notes/${this.props.match.params.id}`);
+  }
+
+  validateForm() {
+    return this.state.content.length > 0;
+  }
+
+  formatFilename(str) {
+    return str.replace(/^\w+-/, "");
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
+  handleFileChange = event => {
+    this.file = event.target.files[0];
+  }
+
+  handleSubmit = async event => {
+    let attachment;
+
+    event.preventDefault();
+
+    if (this.file && this.file.size > config.MAX_ATTACHMENT_SIZE) {
+      alert("Please pick a file smaller than 5MB");
+      return;
+    }
+
+    this.setState({ isLoading: true });
+
+    try {
+      if (this.file) {
+        attachment = await s3Upload(this.file);
+      }
+
+      await this.saveNote({
+        content: this.state.content,
+        attachment: attachment || this.state.note.attachment
+      });
+      this.props.history.push("/");
+    } catch (e) {
+      alert(e);
+      this.setState({ isLoading: false });
+    }
+  }
+
+  handleDelete = async event => {
+    event.preventDefault();
+
+    const confirmed = window.confirm(
+      "Are you sure you want to delete this note?"
+    );
+
+    if (!confirmed) {
+      return;
+    }
+
+    this.setState({ isDeleting: true });
+
+    try {
+      await this.deleteNote();
+      this.props.history.push("/");
+    } catch (e) {
+      alert(e);
+      this.setState({ isDeleting: false });
+    }
+  }
+
+  render() {
+    return (
+      <div className="Notes">
+        {this.state.note &&
+          <form onSubmit={this.handleSubmit}>
+            <FormGroup controlId="content">
+              <FormControl
+                onChange={this.handleChange}
+                value={this.state.content}
+                componentClass="textarea"
+              />
+            </FormGroup>
+            {this.state.note.attachment &&
+              <FormGroup>
+                <ControlLabel>Attachment</ControlLabel>
+                <FormControl.Static>
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href={this.state.attachmentURL}
+                  >
+                    {this.formatFilename(this.state.note.attachment)}
+                  </a>
+                </FormControl.Static>
+              </FormGroup>}
+            <FormGroup controlId="file">
+              {!this.state.note.attachment &&
+                <ControlLabel>Attachment</ControlLabel>}
+              <FormControl onChange={this.handleFileChange} type="file" />
+            </FormGroup>
+            <LoaderButton
+              block
+              bsStyle="primary"
+              bsSize="large"
+              disabled={!this.validateForm()}
+              type="submit"
+              isLoading={this.state.isLoading}
+              text="Save"
+              loadingText="Saving…"
+            />
+            <LoaderButton
+              block
+              bsStyle="danger"
+              bsSize="large"
+              isLoading={this.state.isDeleting}
+              onClick={this.handleDelete}
+              text="Delete"
+              loadingText="Deleting…"
+            />
+          </form>}
+      </div>
+    );
+  }
+}
