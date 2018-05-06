3f71860bae084ac586239b04aec19fe374e65dd1
Jay <jvelayud@gmail.com>
2018-05-05 23:34:05 -0400

    Connecting the billing form

--- src/containers/Settings.js
+++ src/containers/Settings.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import { API } from "aws-amplify";
+import { Elements, StripeProvider } from "react-stripe-elements";
+import BillingForm from "../components/BillingForm";
+import config from "../config";
+import "./Settings.css";
 
 export default class Settings extends Component {
   constructor(props) {
@@ -16,9 +20,39 @@ export default class Settings extends Component {
     });
   }
 
+  handleFormSubmit = async (storage, { token, error }) => {
+    if (error) {
+      alert(error);
+      return;
+    }
+
+    this.setState({ isLoading: true });
+
+    try {
+      await this.billUser({
+        storage,
+        source: token.id
+      });
+
+      alert("Your card has been charged successfully!");
+      this.props.history.push("/");
+    } catch (e) {
+      alert(e);
+      this.setState({ isLoading: false });
+    }
+  }
+
   render() {
     return (
       <div className="Settings">
+        <StripeProvider apiKey={config.STRIPE_KEY}>
+          <Elements>
+            <BillingForm
+              loading={this.state.isLoading}
+              onSubmit={this.handleFormSubmit}
+            />
+          </Elements>
+        </StripeProvider>
       </div>
     );
   }
