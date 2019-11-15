1fc3a823baca09782a33b2468bc0baceed3aa7cf
xuatz <xzlow10@gmail.com>
2019-11-16 02:00:09 +0900

    feat: add `REACT_APP_VERSION` to header
    * downgrade to recommended typescript version
    * add `REACT_APP_VERSION` to header
    * plus necessary changes to config and .env

--- src/components/Navigation/Navigation.style.ts
+++ src/components/Navigation/Navigation.style.ts
@@ -1,6 +1,6 @@
 import { makeStyles } from '@material-ui/styles';
 
-export const useAppStyles = makeStyles({
+export const useStyles = makeStyles({
   appBarHeader: {
     flex: 1,
     textDecoration: 'none',
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -2,16 +2,16 @@ import React, { FC } from 'react';
 import { Link } from 'react-router-dom';
 import { AppBar, Toolbar, Typography } from '@material-ui/core';
 
-import { useAppStyles } from './Navigation.style';
+import { useStyles } from './Navigation.style';
 
 const Navigation: FC = ({ children }) => {
-  const classes = useAppStyles();
+  const classes = useStyles({});
 
   return (
-    <AppBar position="static">
+    <AppBar position='static'>
       <Toolbar>
-        <Typography variant="h6" className={classes.appBarHeader}>
-          <Link to="/">SCRATCH</Link>
+        <Typography variant='h6' className={classes.appBarHeader}>
+          <Link to='/'>{`Scratch - v${process.env.REACT_APP_VERSION}`}</Link>
         </Typography>
         {children}
       </Toolbar>
