3aea2ae39c21693c0a13bb0c1cedf132ae229a76
Jay <jvelayud@gmail.com>
2020-11-06 22:21:17 -0500

    Adding build script

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
-const config = {
+const dev = {
   STRIPE_KEY: "pk_test_v1amvR35uoCNduJfkqGB8RLD",
-  MAX_ATTACHMENT_SIZE: 5000000,
   s3: {
     REGION: "us-east-1",
     BUCKET: "notes-app-2-api-dev-attachmentsbucket-qvdowkch2uj1",
@@ -15,6 +14,31 @@ const config = {
     APP_CLIENT_ID: "5r5o292r1n7s2vgje5u5c7vpq0",
     IDENTITY_POOL_ID: "us-east-1:b718098b-fe72-410b-b4c6-0750f9135672",
   },
+};
+
+const prod = {
+  STRIPE_KEY: "pk_test_v1amvR35uoCNduJfkqGB8RLD",
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-2-api-prod-attachmentsbucket-1v9w7kkxnznb7",
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://api.serverless-stack.seed-demo.club/prod",
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_mLbfKylhm",
+    APP_CLIENT_ID: "mli2vaupiq3ga29m4698m6mrl",
+    IDENTITY_POOL_ID: "us-east-1:4e377eff-0617-4098-b218-673490ffab8d",
+  },
+};
+
+const config = {
+  // Add common config values here
+  MAX_ATTACHMENT_SIZE: 5000000,
+  // Default to dev if not set
+  ...(process.env.REACT_APP_STAGE === "prod" ? prod : dev),
 };
 
 export default config;
