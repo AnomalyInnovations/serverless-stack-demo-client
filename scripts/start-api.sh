3472d8b5add1637b3c0e0c73724bfdf0e33bb295
Danny Paz <dan.pazuchowski@gmail.com>
2022-02-18 14:08:47 -0700

    updates for local api

--- scripts/start-api.sh
+++ scripts/start-api.sh
@@ -8,6 +8,6 @@ echo "Starting the api"
 	cd serverless-stack-demo-api
   docker-compose up -d
   npm install
-  node --experimental-modules scripts/init-table.js
+  node scripts/init-table.js
   npm start
 )
